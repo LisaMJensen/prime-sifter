edff758fcf2af0fdb6e8d1bca65dc0f1ca3da27a
Lisa Jensen and Na Hyung Choi <na@>
2019-07-17 14:08:23 -0700

    successfully return all the numbers less than the input number that are prime

--- Models/ClassName.cs
+++ Models/ClassName.cs
@@ -1,1 +1,30 @@
 using System;
+using System.Collections.Generic;
+
+class Number
+{
+  public static List<int> numbers = new List<int> {};
+
+  public static void PrimeSifter(int inputNumber)
+  {
+    numbers.Clear();
+    // Generates a list of numbers from 2 to the input number
+    for (int i = 2; i <= inputNumber; i++)
+    {
+      numbers.Add(i);
+    }
+
+    // Loops through prime numbers starting with 2, which is numbers[0]
+    for (int i = 0; i < numbers.Count; i++){
+      int prime = numbers[i];
+      // Loop through every number in the list to check if divisible by the current prime number
+      for (int j = 0; j < numbers.Count; j++)
+      {
+        if (numbers[j] % prime == 0 && numbers[j] - prime != 0)
+        {
+          numbers.Remove(numbers[j]);
+        }
+      }
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
 using System;
 
-class ProjectName
+class Program
 {
   static void Main()
   {
-    
+    Console.WriteLine("PRIME SIFTER");
+    Console.WriteLine("Enter a number, and I will give you all the prime numbers less than that number.");
+    Console.WriteLine("Your number:");
+    string answer = Console.ReadLine();
+    int intAnswer = int.Parse(answer);
+
+    Number.PrimeSifter(intAnswer);
+
+    for (int i = 0; i < Number.numbers.Count; i++)
+    {
+      Console.WriteLine(Number.numbers[i]);
+    }
+
+    Main();
   }
 }
